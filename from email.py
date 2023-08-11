3acfd37f3a6aca41cedd83484e76a8e051a1bc31
Voids-Edge <jmwread3@gmail.com>
2023-08-11 16:37:07 -0400

    Create from email.py

---
+++ from email.py
@@ -0,0 +1,23 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = 'jmwread3@gmail.com'
+email_password = 'flydttavbhqlmosb'
+
+email_reciever = 'jmwread3@gmail.com'
+
+subject = input('What type of issue is you ticket about? (deleting, editing, or new document): ')
+body = input("Please describe with as much detail as possible the problem you are having: ")
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_reciever
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_reciever, em.as_string())
